c506c6a83ddf78e1c5c183e65c7552dec4ce5bf7
C46\eztrust <gtobaki520@gmail.com>
2026-09-08 13:13:59 +0800

    2026.9.8企業名錄

--- js/aos-master/src/js/libs/observer.js
+++ js/aos-master/src/js/libs/observer.js
@@ -1,9 +1,39 @@
-const doc = window.document;
-const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
 let callback = () => {};
 
-function ready (selector, fn) {
+function containsAOSNode(nodes) {
+  let i, currentNode, result;
+
+  for (i = 0; i < nodes.length; i += 1) {
+    currentNode = nodes[i];
+
+    if (currentNode.dataset && currentNode.dataset.aos) {
+      return true;
+    }
+
+    result = currentNode.children && containsAOSNode(currentNode.children);
+
+    if (result) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function getMutationObserver() {
+  return window.MutationObserver ||
+    window.WebKitMutationObserver ||
+    window.MozMutationObserver;
+}
+
+function isSupported() {
+  return !!getMutationObserver();
+}
+
+function ready(selector, fn) {
+  const doc = window.document;
+  const MutationObserver = getMutationObserver();
+
   const observer = new MutationObserver(check);
   callback = fn;
 
@@ -18,17 +48,14 @@ function check(mutations) {
   if (!mutations) return;
 
   mutations.forEach(mutation => {
-    const addedNodes = Array.prototype.slice.call(mutation.addedNodes)
-    const removedNodes = Array.prototype.slice.call(mutation.removedNodes)
+    const addedNodes = Array.prototype.slice.call(mutation.addedNodes);
+    const removedNodes = Array.prototype.slice.call(mutation.removedNodes);
+    const allNodes = addedNodes.concat(removedNodes);
 
-    const anyAOSElementAdded = addedNodes.concat(removedNodes)
-      .filter(el => el.hasAttribute && el.hasAttribute('data-aos'))
-      .length
-
-    if(anyAOSElementAdded) {
-      callback()
+    if (containsAOSNode(allNodes)) {
+      return callback();
     }
   });
 }
 
-export default ready;
+export default { isSupported, ready };
